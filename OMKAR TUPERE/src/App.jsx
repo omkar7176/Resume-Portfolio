03c11836b3956b1c63937838593a7c8a56e20237
Omkar Tupere <115610759+omkar7176@users.noreply.github.com>
2025-01-24 11:04:42 +0000

    files has been updated

--- OMKAR TUPERE/src/App.jsx
+++ OMKAR TUPERE/src/App.jsx
@@ -5,23 +5,10 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Technologies from "./components/Technologies";
 import FloatingBar from "./components/FloatingBar";
-// import { ThemeProvider, useTheme } from "./components/ThemeProvider";
 
-// const ThemeToggleButton = () => {
-//   const { toggleTheme } = useTheme();
-//   return (
-//     <button
-//       onClick={toggleTheme}
-//       className="px-4 py-2 rounded bg-gray-800 text-white"
-//     >
-//       Toggle Theme
-//     </button>
-//   );
-// };
 
 const App = () => {
   return (
-    // <ThemeProvider>
     <div className="overflow-x-hidden antialiased">
       <div className="container mx-auto px-8">
         <Navbar />
@@ -31,10 +18,8 @@ const App = () => {
         <Experience />
         <Contact />
         <FloatingBar />
-        {/* <ThemeToggleButton /> */}
       </div>
     </div>
-    // </ThemeProvider>
   );
 };
 
